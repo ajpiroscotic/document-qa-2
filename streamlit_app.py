72d5f1370782c65976e422e358ae7366397744c8
ajpiroscotic <31822666+ajpiroscotic@users.noreply.github.com>
2024-09-05 15:53:16 +0000

    multipager changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 
-lab1 = st.Page("Lab1.py",title="Lab 1")
-lab2 = st.Page("Lab2.py",title="Lab 2", default=True)
+lab1 = st.Page("lab1.py",title="Lab 1")
+lab2 = st.Page("lab2.py",title="Lab 2", default=True)
 pg = st.navigation([lab1, lab2])
 st.set_page_config(page_title="Labs Manager")
 pg.run()
