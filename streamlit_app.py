3a63ec5c937be4b9c00c4e07199646c69ad7e469
ajpiroscotic <31822666+ajpiroscotic@users.noreply.github.com>
2024-09-05 15:48:11 +0000

    multipager

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,8 @@
 import streamlit as st
 
-# Set page config
-st.set_page_config(page_title="IST688_Labs", page_icon="✏️")
 
-# Define pages
-Lab_1 = st.Page("lab1.py", title="Lab 1")
-Lab_2 = st.Page("lab2.py", title="Lab 2")
-
-# Set up navigation
-pages = [Lab_1, Lab_2]
-page = st.navigation(pages)
-
-# Check if a page is selected, otherwise set default
-if page is None:
-    page = Lab_2
-
-# Run the selected page
-page.run()
+lab1 = st.Page("Lab1.py",title="Lab 1")
+lab2 = st.Page("Lab2.py",title="Lab 2", default=True)
+pg = st.navigation([lab1, lab2])
+st.set_page_config(page_title="Labs Manager")
+pg.run()
